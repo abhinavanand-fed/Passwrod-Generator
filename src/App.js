0e6189ae716d19a50f89170af186dff571694943
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-03 18:06:58 +0530

    pw gen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+function PasswordGenerator() {
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(10);
 
-function App() {
+  const generatePassword = () => {
+    const possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#";
+    let generatedPassword = "";
+
+    for (let i = 0; i < length; i++) {
+      generatedPassword += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+    }
+
+    setPassword(generatedPassword);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="password-generator">
+      <h1>Password Generator</h1>
+      <div className="slider-container">
+        <p className="length-label">Password Length: {length}</p>
+        <input 
+          type="range" 
+          min="8" 
+          max="20" 
+          value={length} 
+          onChange={(e) => setLength(e.target.value)} 
+          className="length-slider" 
+        />
+      </div>
+      <div className="password-container">
+        <p className="password">{password}</p>
+        <button className="generate-button" onClick={generatePassword}>Generate Password</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default PasswordGenerator;
